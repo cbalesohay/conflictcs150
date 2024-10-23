e3642131335952af97a5bc8f7acca2a1f0daea06
cbalesohay <kawipro32@gmail.com>
2024-10-23 10:19:38 -0700

    push add function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
-
+int add(int a, int b);
 int main(){
     int hello = 0;
     cout << hello << endl;
     cout << "Hello world" << endl;
-
+    cout "5 + 7 = " << add(5, 7) << endl;
     return 0;
+}
+
+int add(int a, int b){
+    return a + b;
 }
