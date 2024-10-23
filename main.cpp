5a66924fdddbb3e52c0202b083b45ffc4b2a3cbf
Christopher Bailey <125836099+cbalesohay@users.noreply.github.com>
2024-10-23 17:20:35 +0000

    add say function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
-
+void say(string word);
 int main(){
     int hello = 0;
     cout << hello << endl;
     cout << "Hello world" << endl;
-
+    say("hello");
     return 0;
+}
+
+void say(string word){
+    cout << word << endl;
 }
