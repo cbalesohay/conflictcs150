1c501d0d7d6f9bebbc83384e52dcf8054d5de5db
cbalesohay <kawipro32@gmail.com>
2024-10-23 10:24:27 -0700

    saving file changes

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
     int hello = 0;
     cout << hello << endl;
     cout << "Hello world" << endl;
-    cout "5 + 7 = " << add(5, 7) << endl;
+    cout << "5 + 7 = " << add(5, 7) << endl;
     return 0;
 }
 
